45d9c55168e09b0e2df729b3bebeedaa9a97c952
Corey <cor.johnson@gmail.com>
2022-01-18 22:13:09 -0800

    Cleanup some imports

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,34 @@
 use futures::executor::block_on;
-
-/// Audio clips contain a path, the name of the clip, and the duration
-struct AudioClip {
-    name: String,
-    duration: u32,
-    bytes: [u8],
-}
+use std::{
+    fs::File,
+    io::BufReader,
+    thread,
+    time::Duration
+};
 
 /// This function will spin up a thread, play the audio in the background and wait till the
 /// audio has completed it's playback before jumping back to the main thread
+/// 
+/// Note: this currently uses rodio which spawns it's own thread, so I'll need to use a sink
+/// to get the actual duration for the clip
 async fn play_audio() {
-    println!("I'm running in async!");
+    println!("Playing the audio asynchronously!");
 
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-    let file = std::fs::File::open("examples_music.wav").unwrap();
-    let music = stream_handle.play_once(std::io::BufReader::new(file)).unwrap();
+    let file = File::open("examples_music.wav").unwrap();
+    let music = stream_handle.play_once(BufReader::new(file)).unwrap();
     music.set_volume(8.0);
-    std::thread::sleep(std::time::Duration::from_secs(10));
+    thread::sleep(Duration::from_secs(10));
 }
 
 /// Allow the user to select what audio they want to play
 fn select_audio() {
     println!("Please select the audio you wish to play");
+    println!("There are no options available, the default tune will play");
 }
 
 fn main() {
-    println!("Welcome to the audio project!");
+    println!("Welcome to this simple audio player");
 
     let mut app_running_state = true;
     let mut input_string = String::new();
